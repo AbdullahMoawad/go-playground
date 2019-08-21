eeb47faaafd6709b33e474d62056135c203ac92a
AbdullahMoawad <abdalla.mohamed@fly365.com>
2019-08-21 16:30:13 +0200

    fmt files

--- routes/userRoutes.go
+++ routes/userRoutes.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/sql-queries/controller"
 	"net/http"
@@ -13,6 +12,6 @@ func Routes()  {
 	r.HandleFunc("/update", Logging(controller.UpdateUser)).Methods("PUT")
 	r.HandleFunc("/user/login", Logging(controller.Login)).Methods("POST")
 	r.HandleFunc("/user/logout", Logging(controller.Logout)).Methods("PUT")
-	fmt.Println(http.ListenAndServe(":8000", r))
+	http.ListenAndServe(":8000", r)
 }
 
